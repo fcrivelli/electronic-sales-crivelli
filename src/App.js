14ab341d2553a7c1efa3880fcd7cc902b54229af
fcrivelli <cofrta@gmail.com>
2021-06-03 21:01:59 -0300

    navBar+Crivelli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Route from 'react-router-dom/Route'
+import Agregar from './pages/agregar'
+import Home from './pages/home'
+import { BrowserRouter } from 'react-router-dom'
+import Nav from "./components/navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Nav />
+            <Route exact path="/" component={Home} />
+            <Route path="/agregar" component={Agregar} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
