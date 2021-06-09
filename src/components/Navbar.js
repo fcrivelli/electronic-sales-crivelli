3f5dbffd3aa71dfe2aa34d332cabcbe19941fed9
fcrivelli <cofrta@gmail.com>
2021-06-08 23:14:39 -0300

    cartWidgetAndItemList+Crivelli

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,67 @@
 import React from 'react';
-import {NavLink, withRouter}  from 'react-router-dom'
+import {Link, withRouter}  from 'react-router-dom'
 class Navbar extends React.Component {
+    /*
     getNavLinkClass = (path) => {
         return this.props.location.pathname === path ? 'active' : '';
-    }
+    }*/
     render() {
         return (
+            <nav
+                className="navbar container"
+                role="navigation"
+                aria-label="main navigation"
+            >
+            <div className="navbar-brand">
+              <b className="navbar-item is-size-4 ">ecommerce</b>
+              <label
+                    role="button"
+                    class="navbar-burger burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample"
+                    onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                    }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </label>
+            </div>
+              <div className={`navbar-menu ${
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
+                  Cart
+                  <span
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
+                  >
+                    { Object.keys(this.state.cart).length }
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to="/login" className="navbar-item">
+                    Login
+                  </Link>
+                ) : (
+                  <Link to="/" onClick={this.logout} className="navbar-item">
+                    Logout
+                  </Link>
+                )}
+              </div>
+            </nav>
+            /*
             <nav className="navbar navbar-inverse" >
                 <div className="container-fluid">
                     <div className="navbar-header">
@@ -24,9 +80,9 @@ class Navbar extends React.Component {
                         </ul>
                     </div>
                 </div>
-            </nav>
+            </nav>*/
         )
     }
 };
-Navbar = withRouter(Navbar);
+//Navbar = withRouter(Navbar);
 export default Navbar;
