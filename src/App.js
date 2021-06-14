6fa67c15881a00e52c1780e6887ac96395497afb
fcrivelli <cofrta@gmail.com>
2021-06-13 21:45:09 -0300

    AddNavbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Context from "./Context"
-import AddProduct from './components/AddProduct';
-import Cart from './components/Cart';
-import Login from './components/Login';
-import ProductList from './components/ProductList';
+import Navbar from './components/Navbar';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
@@ -96,40 +93,7 @@ export default class App extends Component {
     >
       <Router ref={this.routerRef}>
         <div className="App">
-          <nav className="navbar container" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-              <b className="navbar-item is-size-4 ">Electronic-Sales</b>
-              <label role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"
-                onClick={e => {
-                  e.preventDefault();
-                  this.setState({ showMenu: !this.state.showMenu });
-                }}
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </label>
-            </div>
-            <div className={`navbar-menu ${
-                this.state.showMenu ? "is-active" : ""
-              }`}>
-              <Link to="/products" className="navbar-item">Products</Link>
-              <Link to="/add-product" className="navbar-item">Add Product</Link>
-              <Link to="/cart" className="navbar-item">Cart
-                <span className="tag is-primary" style={{ marginLeft: "5px" }}> { Object.keys(this.state.cart).length }</span>
-              </Link>
-              {!this.state.user ? (<Link to="/login" className="navbar-item">Login</Link>) : (
-                <Link to="/" onClick={this.logout} className="navbar-item">Logout</Link>
-              )}
-            </div>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={ProductList} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/add-product" component={AddProduct} />
-            <Route exact path="/products" component={ProductList} />
-          </Switch>
+          <Navbar/>
         </div>
       </Router>
     </Context.Provider>
